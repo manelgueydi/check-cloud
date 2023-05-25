3d9f97346557c0d23a505ade520182b726a3b70e
manelgueydi <manelgaidi9999@gmail.com>
2023-05-25 21:27:36 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import IntroSection from "./components/intro/Intro";
+import ContactSection from "./components/contact-section/ContactSection";
+import DisclaimerSection from "./components/disclaimer/Disclaimer";
+import FooterSection from "./components/footer/Footer";
+
+import "./App.css";
+import Map from "./components/Map/Map";
+
+function App() {
+  const location = {
+    
+  //GOMYCODE_Hackerspace Béja
+  
+  Address: "Immeuble Bedouihech, 2ème étage, rue Rahmani Mnakbi",
+  Latitude: 9.188522786921679,
+  Longtude: 36.724479096550986, 
+  };
+  
+  return (
+    <div className="App">
+      <IntroSection />
+      <div>
+        <Map location={location} zoomLevel={16} />
+      </div>
+      <ContactSection />
+      <DisclaimerSection />
+      <FooterSection />
+    </div>
+  );
+}
+
+export default App;
+
